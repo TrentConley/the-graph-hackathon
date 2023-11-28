65f616789bc254af448a3f2db982c03b4c48fdea
Trent Conley <trentconley@gmail.com>
2023-11-27 23:15:31 -0500

    got sale and price

--- the-graph-hackathon/src/components/SearchBar.tsx
+++ the-graph-hackathon/src/components/SearchBar.tsx
@@ -1,31 +1,41 @@
-import React, { useState } from 'react';
-import { getPunkDetails } from './query';
-
+import React, { useState } from "react";
+import { getPunkDetails } from "./query";
 
 interface SearchBarProps {
-    onSearch: (id: string) => void; // Function to handle the search
+  onSearch: (id: string) => void; // Function to handle the search
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
-    const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
-    const handleSearch = () => {
-        console.log('searching');
-        getPunkDetails(inputValue).then(data => console.log(data)).catch(error => console.error(error));
-         console.log('finished search');
-    };
+  const handleSearch = async () => {
+    console.log("searching");
+    const data = await getPunkDetails(inputValue);
+    if (data && data.punk) {
+      data.punk.events.forEach((event: any) => {
+        if (event.type === "SALE") {
+          const saleTimestamp = parseInt(event.timestamp, 10);
+          const saleDate = new Date(saleTimestamp * 1000); // Converting Unix timestamp to JavaScript Date object
+          const formattedDate = saleDate.toISOString().split("T")[0]; // Formatting to YYYY-MM-DD
 
-    return (
-        <div>
-            <input
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                placeholder="Enter CryptoPunk ID"
-            />
-            <button onClick={handleSearch}>Search</button>
-        </div>
-    );
+          console.log(`Sale on ${formattedDate}, Amount: ${event.amount}`);
+        }
+      });
+    }
+    console.log("finished search");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter CryptoPunk ID"
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
 };
 
 export default SearchBar;
--- the-graph-hackathon/src/components/query.ts
+++ the-graph-hackathon/src/components/query.ts
@@ -1,51 +1,35 @@
 // queries.ts
-import { gql } from '@apollo/client';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-
-
-export function getPunkDetails(punkID: string) {
+export async function getPunkDetails(punkID: string) {
   console.log(punkID);
   const client = new ApolloClient({
-    uri: 'https://gateway-arbitrum.network.thegraph.com/api/d1357ccb340846b7365b0da4215c44ee/subgraphs/id/2hTKKMwLsdfJm9N7gUeajkgg8sdJwky56Zpkvg8ZcyP8', // replace with your GraphQL server URI
-    cache: new InMemoryCache()
+    uri: "https://gateway-arbitrum.network.thegraph.com/api/d1357ccb340846b7365b0da4215c44ee/subgraphs/id/2hTKKMwLsdfJm9N7gUeajkgg8sdJwky56Zpkvg8ZcyP8", // replace with your GraphQL server URI
+    cache: new InMemoryCache(),
   });
   const GET_PUNK_DETAILS = gql`
-  query GetPunkDetails($punkID: ID!) {
-    punk(id: $punkID) {
-      id
-      tokenId
-      owner {
-        id
-      }
-      numberOfTransfers
-      numberOfSales
-      events {
-        id
-        type
-        from {
-          id
+    query GetPunkDetails($punkID: ID!) {
+      punk(id: $punkID) {
+        events {
+          type
+          timestamp
+          amount
+          from {
+            id
+          }
+          to {
+            id
+          }
         }
-        to {
-          id
-        }
-        amount
-        blockNumber
-        timestamp
-      }
-      currentAsk {
-        amount
-      }
-      currentBid {
-        amount
       }
     }
-  }   
-`;
-return client.query({
-    query: GET_PUNK_DETAILS,
-    variables: { punkID },
-})
-.then(response => response.data)
-.catch(error => console.error(error));
+  `;
+  return client
+    .query({
+      query: GET_PUNK_DETAILS,
+      variables: { punkID },
+    })
+    .then((response) => response.data)
+    .catch((error) => console.error(error));
 }
